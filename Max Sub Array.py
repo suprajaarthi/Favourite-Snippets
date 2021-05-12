512b45276c94c50dc351af57a1b375a21c94fc48
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-12 21:36:26 +0530

    Create Max Sub Array.py
    https://ide.geeksforgeeks.org/DBOI5KKryH

---
+++ Max Sub Array.py
@@ -0,0 +1,17 @@
+
+# Given an integer array nums, find the contiguous subarray 
+# (containing at least one number) which has the largest sum and return its sum.
+
+
+nums=list(map(int,input().split()))
+sumVal = ret = 0
+for i in nums: 
+    sumVal = max(0, sumVal) + i 
+    # print(sumVal)
+    ret = max(ret, sumVal) 
+    # print(ret)
+print(max(nums) if ret == 0 else ret)
+
+
+# Input: [-2,1,-3,4,-1,2,1,-5,4],
+#     https://ide.geeksforgeeks.org/DBOI5KKryH
