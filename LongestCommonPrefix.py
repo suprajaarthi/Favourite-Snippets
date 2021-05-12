c2b1bd80fd3d61d473f595b6b359630331e89998
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-13 00:09:16 +0530

    Create LongestCommonPrefix.py

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,14 @@
+def common(a,b):
+    i=0;la=len(a);lb=len(b)
+    while(i<la and i<lb and a[i]==b[i]):
+        i+=1
+    return a[:i]
+    
+l=input().strip().split()
+s=l[0]
+# print(s)
+for i in l:
+    s=common(s,i)
+print(s)
+
+# https://ide.geeksforgeeks.org/Rm5qsOy5Xa
