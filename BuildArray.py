8526d01bc15ef33cdd51dc730d49f9466fd3db59
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-14 12:36:08 +0530

    Create BuildArray.py
    Build Array with Stack operations

---
+++ BuildArray.py
@@ -0,0 +1,28 @@
+target=list(map(int,input().split()))
+n=int(input())
+res, pointer = [], 0
+        
+for val in range(1, n+1):
+    if pointer == len(target):
+        break
+    elif val == target[pointer]:
+        res += ['Push']
+        pointer += 1
+    else:
+        res += ["Push", "Pop"]
+
+print(res)
+
+
+# 1 3 
+# push 1 
+# push 2 pop 2 
+# push 3 
+# => [ push, push , pop, push]
+
+# https://ide.geeksforgeeks.org/MHcICyNWb1
+  
+  
+  
+  
+  
