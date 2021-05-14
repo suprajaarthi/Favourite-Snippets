01c5666e576c6b0dead75aff8b993906b75a08ee
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-14 12:54:56 +0530

    Create ClockwiseRotateMatrix.py

---
+++ ClockwiseRotateMatrix.py
@@ -0,0 +1,23 @@
+# https://ide.geeksforgeeks.org/knfsQzfInC
+
+#Your code below
+n=int(input())
+c=int(input())
+a=[list(map(int,input().split())) for i in range(n)]
+a=[*zip(*a)]
+for i in a:
+    print(*i)
+
+    
+ip: 
+3
+3
+1 2 3 
+4 5 6
+7 8 9 
+
+op:
+1 4 7
+2 5 8
+3 6 9
+
