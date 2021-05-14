4449a176e3e247dae68506560d1e9d17e40bfd7e
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-05-14 13:28:27 +0530

    Create MaxSum.py

---
+++ MaxSum.py
@@ -0,0 +1,13 @@
+
+l=list(map(int,input().split()))
+maxsum=l[0];curr=l[0]
+for i in l[1:]:
+    curr=max(i,curr+i)
+    maxsum=max(curr,maxsum)
+print(maxsum) 
+
+# IP : -2 1 -3 4 -1 2 1 -5 4 
+# EXPLANATION : 4 -1 2 1 => 6
+# OP : 6
+
+# https://ide.geeksforgeeks.org/sBYbm5bGlC
