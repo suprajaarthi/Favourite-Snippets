cfd44a0bc99b384513b3fdeaff60a1ec699a1492
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-09 22:00:00 +0530

    Create PermutationItertool.py

---
+++ PermutationItertool.py
@@ -0,0 +1,14 @@
+# https://ide.geeksforgeeks.org/aekdGyPch2
+from itertools import permutations 
+a = "GeEK"
+
+p = permutations(a) 
+for j in list(p): 
+    print(j) 
+    
+# ('G', 'E', 'K')
+# ('G', 'K', 'E')
+# ('E', 'G', 'K')
+# ('E', 'K', 'G')
+# ('K', 'G', 'E')
+# ('K', 'E', 'G')
