761f5b5405c10443424c60ec7bd9a11ecb272823
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-10 23:55:52 +0530

    Update InvertedPattern.py

--- InvertedPattern.py
+++ InvertedPattern.py
@@ -1,3 +1,5 @@
+    https://ide.geeksforgeeks.org/3eaBhqW0kD
+
 a=int(input())
 k=1
 for i in range(1,a+1):
@@ -11,7 +13,21 @@ for i in range(a,0,-1):
 	    print(k,end=' ')
 	    k-=1
     print("")
+
+IP 5 
+OP 
+
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+15 14 13 12 11 
+10 9 8 7 
+6 5 4 
+3 2 
+1 
+
     
     
     
-#     https://ide.geeksforgeeks.org/3eaBhqW0kD
