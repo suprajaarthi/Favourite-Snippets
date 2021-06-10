f06ec49f2bfca696526ffe390edf28c3d06872f7
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-11 00:11:14 +0530

    Update RowCol0.py

--- RowCol0.py
+++ RowCol0.py
@@ -1,6 +1,8 @@
 # Replace Entire Row or Column which has 0
 
-
+https://ide.geeksforgeeks.org/m38F9h3SVX 
+    
+    
 n,m=map(int,input().split())
 l=[list(map(int,input().split())) for i in range(n)]
 x,y=[],[]    
@@ -16,3 +18,16 @@ for i in range(n):
         else:
             print(l[i][j],end=" ")
     print()     
+
+IP
+
+3 3
+1 2 3 
+4 5 0
+7 0 9
+
+OP 
+
+1 0 0 
+0 0 0 
+0 0 0 
