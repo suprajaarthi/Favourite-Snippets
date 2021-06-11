d69dbf68b0c0eaf3bb9ca711c0a2e55883736911
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-11 09:27:00 +0530

    Print Matrix in zigzag reverse form

--- zigzag rev.py
+++ zigzag rev.py
@@ -1,4 +1,5 @@
-#Your code below
+https://ide.geeksforgeeks.org/etcum3PBQh
+    
 n=int(input())
 l=[list(map(int,input().split())) for i in range(n)]
 # print(*l)
@@ -9,4 +10,20 @@ for i in range(n-1,-1,-1):
     else:
         print(*l[i][::-1])
     leftoryt=not leftoryt
+
+IP 
+
+3
+1 2 3
+4 5 6 
+7 8 9 
+
+
+OP 
+
+7 8 9
+6 5 4
+1 2 3
+
+
 
