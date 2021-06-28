48ec4b3a20f88d3ad3614bd3a6494a320c95e75c
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-06-28 22:38:25 +0530

    Update FirstUniq.py

--- FirstUniq.py
+++ FirstUniq.py
@@ -1,4 +1,5 @@
-
+https://ide.geeksforgeeks.org/7Wh0HY87ES
+    
 # Return index of 1st unique char in string
 
 s=input().strip()
@@ -11,7 +12,8 @@ for i in range(len(s)):
             quit()
             
 print("-1")
-
+IP : apples 
+OP : 3
 # reduce running time :add in set and if not pres already check for first time 
 # if pres in set no need to check bcoz there is no possib that the count is 1
 # simply s.count(i) will give res but this red time complexity
